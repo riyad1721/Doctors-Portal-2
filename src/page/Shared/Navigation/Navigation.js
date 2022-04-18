db0cd60654e712a0bacc44d84f7161b1265c14cd
riyad1721 <reyadhasan7254@gmail.com>
2022-04-18 20:54:44 +0600

    first upload

---
+++ src/page/Shared/Navigation/Navigation.js
@@ -0,0 +1,31 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+
+
+
+const Navigation = () => {
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Navigation;
