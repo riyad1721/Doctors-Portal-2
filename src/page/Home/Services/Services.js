16b0e14c2228e2cb58c28d5739cf659d74e8c476
riyad1721 <reyadhasan7254@gmail.com>
2022-04-24 22:00:02 +0600

    service section start

---
+++ src/page/Home/Services/Services.js
@@ -0,0 +1,23 @@
+import { Container, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+
+const Services = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Container>
+                <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} variant="h6" component="div">
+                    OUR SERVICES
+                </Typography>
+                <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
+                    Services We Provide
+                </Typography>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
+
+export default Services;
